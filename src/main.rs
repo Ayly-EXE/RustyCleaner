59225157d231fbafe0673bb55ecb95ba8dafb4d3
Ayly_EXE <80326459+Ayly-EXE@users.noreply.github.com>
2026-05-15 10:13:23 +0200

    Added the Ratatui TUI

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,45 @@ use std::path::{Path, PathBuf};
 use std::thread;
 use std::time::Duration;
 use clap::Parser;
+use crossterm::event;
+use crossterm::event::{Event, KeyCode};
 use fs_extra::dir::get_size;
+use ratatui::Frame;
+use ratatui::layout::Constraint::{Fill, Length};
+use ratatui::layout::Layout;
+use ratatui::style::{Color, Modifier, Style};
+use ratatui::text::{Line, Text};
+use ratatui::widgets::{Block, Borders, List, ListItem, ListState, Paragraph};
 
-static TARGETS: [&str; 12] = [
+
+//  TODO :
+//  - todo with tab - DONE
+//  - todo make a mew tab with Delete X files and save [xxx] yB
+//  - todo with delete with confirmation
+//  - todo helper at the bottom with commands
+//  - todo if possible remove scan complete, or make it into the tui app (not sure yet)
+//  - todo clean up if possible in multiple files
+
+
+enum Selection {
+    Category(usize),
+    Path(usize, usize),
+}
+
+static TARGETS: [&str; 11] = [
     "target",
     ".embuild",
     "venv", ".venv", "env", ".env",
     "node_modules",
-    "__pycache__",
     "_build", "deps",
     "vendor",
     "dist",
 ];
+
 #[derive(Parser)]
 struct Cli {
     path: Option<PathBuf>,
 }
-
 
 fn detect_language(entry_path: &Path, file_name: &str) -> Option<&'static str> {
     let parent = entry_path.parent();
@@ -32,13 +54,12 @@ fn detect_language(entry_path: &Path, file_name: &str) -> Option<&'static str> {
         "_build" | "deps" => Some("Elixir"),
         ".stack-work" | "dist-newstyle" => Some("Haskell"),
         "Pods"          => Some("Swift"),
-
         "target" => {
-            if      has("Cargo.toml")    { Some("Rust") }
-            else if has("pom.xml")       { Some("Java (Maven)") }
-            else if has("build.gradle")  { Some("Java (Gradle)") }
-            else if has("build.sbt")     { Some("Scala") }
-            else                         { None }
+            if      has("Cargo.toml")   { Some("Rust") }
+            else if has("pom.xml")      { Some("Java (Maven)") }
+            else if has("build.gradle") { Some("Java (Gradle)") }
+            else if has("build.sbt")    { Some("Scala") }
+            else                        { None }
         }
         "build" => {
             if      has("build.gradle")  { Some("Java (Gradle)") }
@@ -58,7 +79,6 @@ fn detect_language(entry_path: &Path, file_name: &str) -> Option<&'static str> {
             else if has("pyproject.toml") || has("setup.py") { Some("Python") }
             else                         { None }
         }
-
         _ => None,
     }
 }
@@ -80,14 +100,13 @@ fn lookup(file_map: &mut HashMap<String, Vec<PathBuf>>, path: &Path) {
         let is_target = TARGETS.contains(&file_name);
 
         if is_target && entry_path.is_dir() {
-
-            let key = detect_language(&entry_path, file_name).unwrap_or(file_name).to_string();
-
+            let key = detect_language(&entry_path, file_name)
+                .unwrap_or(file_name)
+                .to_string();
             file_map
                 .entry(key)
                 .or_insert_with(Vec::new)
                 .push(entry_path.clone());
-
             continue;
         }
 
@@ -97,13 +116,271 @@ fn lookup(file_map: &mut HashMap<String, Vec<PathBuf>>, path: &Path) {
     }
 }
 
-fn main() {
+fn format_size(size: f32) -> String {
+    if size > 1_073_741_824.0 {
+        format!("[{:>10.2} GB]", size / 1_073_741_824.0)
+    } else if size > 1_048_576.0 {
+        format!("[{:>10.2} MB]", size / 1_048_576.0)
+    } else if size > 1_024.0 {
+        format!("[{:>10.2} KB]", size / 1_024.0)
+    } else {
+        format!("[{:>10.2} B ]", size)
+    }
+}
+
+fn to_flat_index(selection: &Selection) -> usize {
+    match selection {
+        Selection::Category(i) => *i,
+        Selection::Path(i, j) => i + 1 + j,
+    }
+}
+
+fn build_list_items<'a>(
+    file_map: &'a HashMap<String, Vec<PathBuf>>,
+    category_size: &'a HashMap<String, f32>,
+    file_size: &'a HashMap<PathBuf, u64>,
+    selection: &Selection,
+    is_expanded: bool,
+    to_delete: &Vec<PathBuf>,
+) -> Vec<ListItem<'a>> {
+    let mut items: Vec<ListItem> = Vec::new();
+    let mut keys: Vec<&String> = file_map.keys().collect();
+    keys.sort();
+
+    let expanded_category = match selection {
+        Selection::Category(i) => *i,
+        Selection::Path(i, _) => *i,
+    };
+
+    for (i, key) in keys.iter().enumerate() {
+        let size = category_size.get(*key).unwrap_or(&0.0);
+        let is_selected = expanded_category == i;
+        let arrow = if is_expanded && is_selected { "▼" } else { "▶" };
+
+        items.push(ListItem::new(format!(
+            " {} {:<20} {}",
+            arrow, key, format_size(*size)
+        )));
+
+        if is_expanded && is_selected {
+            for path in &file_map[*key] {
+                let path_size = *file_size.get(path).unwrap_or(&0) as f32;
+
+                let item_style = if to_delete.contains(path) {
+                    Style::default().fg(Color::Red)
+                } else {
+                    Style::default().fg(Color::Green)
+                };
+
+                items.push(
+                    ListItem::new(format!(
+                        "      {} {}",
+                        format_size(path_size),
+                        path.display()
+                    ))
+                        .style(item_style),
+                );
+            }
+        }
+    }
+
+    items
+}
+
+fn render(
+    frame: &mut Frame,
+    file_map: &HashMap<String, Vec<PathBuf>>,
+    category_size: &HashMap<String, f32>,
+    file_size: &HashMap<PathBuf, u64>,
+    list_state: &mut ListState,
+    is_expanded: bool,
+    selection: &Selection,
+    to_delete: &Vec<PathBuf>,
+    to_delete_size:f32,
+) {
+    let bg = Block::default().style(Style::default().bg(Color::Rgb(22, 27, 51)));
+
+    let [outer_layer] = Layout::vertical([Fill(1)]).areas(frame.area());
+
+    let outside_box = Block::default()
+        .title("Rusty Cleaner v0.2.1")
+        .title_style(Style::default().fg(Color::Rgb(158, 42, 43)));
+
+    let inner_area = outside_box.inner(outer_layer);
+    let [top_layer, selection_layer, confirm_layer] = Layout::vertical([Length(3), Fill(10), Length(5)]).areas(inner_area);
+
+    let top_box = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default().fg(Color::Rgb(51, 92, 103)));
+
+    let selection_box = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default().fg(Color::Rgb(224, 159, 62)));
+
+    let confirm_box = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default().fg(Color::Rgb(51, 92, 103)));
+
+    let top_text = Paragraph::new(format!(
+        " TOTAL 'WASTED' SPACE : {}",
+        format_size(*category_size.get("total").unwrap_or(&0.0))
+    ))
+        .style(Style::default().fg(Color::Rgb(229, 229, 229)).add_modifier(Modifier::BOLD))
+        .block(top_box);
+
+    let items = build_list_items(file_map, category_size, file_size, selection, is_expanded, to_delete);
+
+    let list = List::new(items)
+        .block(selection_box)
+        .highlight_style(Style::default().fg(Color::Yellow).add_modifier(Modifier::BOLD))
+        .highlight_symbol(">> ");
+    
+    
+    let free_space = Text::from(vec![
+        Line::from(format!("TOTAL RECLAIMED SPACE : {}", format_size(to_delete_size))),
+        Line::from(format!("{} FOLDERS SELECTED", to_delete.len()))
+    ]
+    );
+    
+    
+
+    let free_space_text = Paragraph::new(free_space)
+        .style(Style::default().fg(Color::Rgb(229, 229, 229)).add_modifier(Modifier::BOLD))
+        .block(confirm_box);
+
+    frame.render_widget(bg, frame.area());
+    frame.render_widget(outside_box, outer_layer);
+    frame.render_widget(top_text, top_layer);
+    frame.render_stateful_widget(list, selection_layer, list_state);
+    frame.render_widget(free_space_text,confirm_layer);
+
+}
+
+fn run_app(
+    terminal: &mut ratatui::DefaultTerminal,
+    file_map: &HashMap<String, Vec<PathBuf>>,
+    category_size: &HashMap<String, f32>,
+    file_size: &HashMap<PathBuf, u64>,
+    to_delete: &mut Vec<PathBuf>,
+) -> std::io::Result<()> {
+    let mut list_state = ListState::default();
+    let mut is_expanded = false;
+    let mut selection = Selection::Category(0);
+
+
+
+    loop {
+
+        let to_delete_size: f32 = to_delete
+            .iter()
+            .map(|p| *file_size.get(p).unwrap_or(&0) as f32)
+            .sum();
+
+        let mut keys: Vec<&String> = file_map.keys().collect();
+        keys.sort();
+
+        let flat = to_flat_index(&selection);
+        list_state.select(Some(flat));
+
+        terminal.draw(|frame| render(
+            frame,
+            file_map,
+            category_size,
+            file_size,
+            &mut list_state,
+            is_expanded,
+            &selection,
+            to_delete,
+            to_delete_size
+        ))?;
+
+        if event::poll(Duration::from_millis(100))? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => return Ok(()),
+
+                    KeyCode::Right => is_expanded = true,
+                    KeyCode::Left => {
+                        match &selection {
+                            Selection::Path(i, _) => selection = Selection::Category(*i),
+                            Selection::Category(_) => is_expanded = false,
+                        }
+                    }
+
+                    KeyCode::Down => {
+                        match &selection {
+                            Selection::Category(i) => {
+                                if is_expanded {
+                                    selection = Selection::Path(*i, 0);
+                                } else {
+                                    selection = Selection::Category((i + 1).min(keys.len() - 1));
+                                }
+                            }
+                            Selection::Path(i, j) => {
+                                let paths_len = file_map[keys[*i]].len();
+                                if j + 1 < paths_len {
+                                    selection = Selection::Path(*i, j + 1);
+                                } else {
+                                    selection = Selection::Category((i + 1).min(keys.len() - 1));
+                                    is_expanded = false;
+                                }
+                            }
+                        }
+                    }
+
+                    KeyCode::Up => {
+                        match &selection {
+                            Selection::Category(i) => {
+                                selection = Selection::Category(i.saturating_sub(1));
+                            }
+                            Selection::Path(i, j) => {
+                                if *j == 0 {
+                                    selection = Selection::Category(*i);
+                                } else {
+                                    selection = Selection::Path(*i, j - 1);
+                                }
+                            }
+                        }
+                    }
+
+                    KeyCode::Tab => {
+                        match &selection {
+                            Selection::Category(i) => {
+                                selection = Selection::Category((i + 1) % keys.len());
+                            }
+                            _ => {}
+                        }
+
+                    }
+
+                    KeyCode::Char(' ') => {
+                        match &selection {
+                            Selection::Path(i, j) => {
+                                let mut keys: Vec<&String> = file_map.keys().collect();
+                                keys.sort();
+                                let path = file_map[keys[*i]][*j].clone();
+
+                                if to_delete.contains(&path) {
+                                    to_delete.retain(|p| p != &path);
+                                } else {
+                                    to_delete.push(path);
+                                }
+                            }
+                            Selection::Category(_) => {}
+                        }
+                    }
+
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
     let args = Cli::parse();
 
-    let start_path = args
-        .path
-        .clone()
-        .unwrap_or_else(|| PathBuf::from("."));
+    let start_path = args.path.clone().unwrap_or_else(|| PathBuf::from("."));
 
     let handle = thread::spawn(move || {
         let mut file_map: HashMap<String, Vec<PathBuf>> = HashMap::new();
@@ -116,14 +393,13 @@ fn main() {
 
     while !handle.is_finished() {
         print!("\rScanning {} ", spinner[i % spinner.len()]);
-        std::io::Write::flush(&mut std::io::stdout()).unwrap();
+        std::io::Write::flush(&mut std::io::stdout())?;
         i += 1;
         thread::sleep(Duration::from_millis(100));
     }
     println!("\rScan completed!   ");
 
     let file_map = handle.join().unwrap();
-
     let all_paths: Vec<PathBuf> = file_map.values().flatten().cloned().collect();
 
     let size_handle = thread::spawn(move || {
@@ -139,7 +415,7 @@ fn main() {
 
     while !size_handle.is_finished() {
         print!("\rCalculating size {} ", spinner[i % spinner.len()]);
-        std::io::Write::flush(&mut std::io::stdout()).unwrap();
+        std::io::Write::flush(&mut std::io::stdout())?;
         i += 1;
         thread::sleep(Duration::from_millis(100));
     }
@@ -147,23 +423,26 @@ fn main() {
 
     let file_size = size_handle.join().unwrap();
 
-    println!("Results:");
+    let mut global_size: f32 = 0.0;
+    let mut category_size: HashMap<String, f32> = HashMap::new();
+
     for (key, paths) in &file_map {
-        println!("\n[{}]:", key);
+        let mut total_size: f32 = 0.0;
         for path in paths {
             let size = *file_size.get(path).unwrap_or(&0) as f32;
-
-            let size_print = if size > 1_073_741_824.0 {
-                format!("[{:>10.2} GB]", size / 1_073_741_824.0)
-            } else if size > 1_048_576.0 {
-                format!("[{:>10.2} MB]", size / 1_048_576.0)
-            } else if size > 1_024.0 {
-                format!("[{:>10.2} KB]", size / 1_024.0)
-            } else {
-                format!("[{:>10.2} B ]", size)
-            };
-
-            println!("  {:<10} {}", size_print , path.display());
+            total_size += size;
         }
+        category_size.insert(key.to_string(), total_size);
+        global_size += total_size;
     }
+
+    category_size.insert("total".to_string(), global_size);
+
+    let mut to_delete: Vec<PathBuf> = Vec::new();
+
+    let mut terminal = ratatui::init();
+    let result = run_app(&mut terminal, &file_map, &category_size, &file_size, &mut to_delete);
+    ratatui::restore();
+
+    result
 }
